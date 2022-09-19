82794184dfb16b9113fa092b7975421b5757d630
bhagigithubnerkar <bhagyashriyeole8796@gmail.com>
2022-09-19 16:11:40 +0530

    Bhagyashri

---
+++ src/Common/Addition.java
@@ -0,0 +1,9 @@
+package Common;
+
+public class Addition {
+    int a;
+    int b;
+    public static void main(String args[]){
+
+    }
+}
---
+++ src/Common/Bhagyashri.java
@@ -0,0 +1,7 @@
+package Common;
+
+public class Bhagyashri {
+    public static void main(String args[]){
+        System.out.println("Hello World");
+    }
+}
